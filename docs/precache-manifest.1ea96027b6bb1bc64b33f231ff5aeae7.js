fe6011090b614d0d7f30469902e81320d734b114
Diego Aragón Duarte <diego_isw@hotmail.com>
2020-08-27 19:43:49 -0500

    Docs generated

---
+++ docs/precache-manifest.1ea96027b6bb1bc64b33f231ff5aeae7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53035d237e6eb3f5bb112afcd12a2928",
+    "url": "/index.html"
+  },
+  {
+    "revision": "41c0ebc77d84dd96e0f0",
+    "url": "/static/css/main.75522443.chunk.css"
+  },
+  {
+    "revision": "12833a1bb5cf6ff1c6cb",
+    "url": "/static/js/2.84cbed32.chunk.js"
+  },
+  {
+    "revision": "1d79dec11c8855ea6437d7f3f5a386b4",
+    "url": "/static/js/2.84cbed32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41c0ebc77d84dd96e0f0",
+    "url": "/static/js/main.b88367c2.chunk.js"
+  },
+  {
+    "revision": "0f7a69e62b3aacc7cf36",
+    "url": "/static/js/runtime-main.8dee5a73.js"
+  }
+]);
